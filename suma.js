7cd511dc48be9fd23dfeed439556ee9f1bf7bb48
leogeney <ljgeneyl@ufpso.edu.co>
2024-01-23 22:53:01 -0500

    primer commit

---
+++ suma.js
@@ -0,0 +1,13 @@
+function sumar(){
+
+    var numero1 = document.getElementById('numero1').value;
+    
+
+    resultadoElement.innerHTML = `<p>La suma es: ${resultado}</p>`;
+
+
+    if(numero1 ="Leonardo"){
+
+        resultadoElement.innerHTML = `<p>100%</p>`;
+    }
+}
